895addfbb721e01370775ef36c2144936332f2ac
Jessica McDonald <jessica.mcdonald@ttu.edu>
2017-11-11 16:33:21 -0600

    added incorrect direction calculation LOL

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
 def wind_speed(u,v,w = 0):
     ''' calculates wind speed from u and v, and w components.'''
     return sqrt(u * u + v * v + w * w)
+
+def get_wind_direction(u,v):
+    return 90 - atan2(u,v, degrees=True)
